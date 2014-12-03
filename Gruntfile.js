7a746553239fa227968a04c96df231491444988a
Oleg Makiienko <oleg.makiienko@gmail.com>
2014-12-03 22:42:09 +0100

    Grunt app packaged

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,24 @@ module.exports = function(grunt) {
         }
       }
     },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/* Unword minified css file */'
+        },
+        files: {
+          'dist/styles/background.min.css': [
+            'app/bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'app/bower_components/angular/angular-csp.css',
+            'app/styles/background.css'
+          ],
+          'dist/styles/popup.min.css': [
+            'app/bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'app/styles/popup.css'
+          ]
+        }
+      }
+    },
     uglify: {
       options: {
         mangle: {
@@ -21,16 +39,40 @@ module.exports = function(grunt) {
         },
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      my_target: {
+      main: {
         files: {
+          'dist/main.js': ['app/main.js'],
+          'dist/js/models.js': ['app/js/models.js'],
+          'dist/js/notification.js': ['app/js/notification.js'],
+          'dist/js/storage.js': ['app/js/storage.js'],
           'dist/js/popup.min.js': [
-            'bower_components/jquery/dist/jquery.min.js',
-            'bower_components/bootstrap/dist/js/bootstrap.min.js',
+            'app/bower_components/jquery/dist/jquery.min.js',
+            'app/bower_components/bootstrap/dist/js/bootstrap.min.js',
             'app/js/storage.js',
             'app/js/notification.js',
             'app/js/models.js',
             'app/js/popup.js',
             'app/js/popup-page.js'
+          ],
+          'dist/js/background.min.js': [
+            'app/bower_components/jquery/dist/jquery.min.js',
+            'app/bower_components/bootstrap/dist/js/bootstrap.min.js',
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            // lib
+            'app/lib/papaparse.js',
+            'app/js/storage.js',
+            'app/js/notification.js',
+            'app/js/models.js',
+            // angular
+            'app/js/unword/app.js',
+            'app/js/unword/routes.js',
+            'app/js/unword/controllers/vocabularies.controller.js',
+            'app/js/unword/controllers/questions.controller.js',
+            'app/js/unword/controllers/config.controller.js',
+            'app/js/unword/services/questions.service.js',
+            'app/js/unword/services/vocabularies.service.js',
+            'app/js/unword/services/dialogs.service.js',
           ]
         }
       }
@@ -39,7 +81,8 @@ module.exports = function(grunt) {
       main: {
         files: [
           {expand: true, cwd: 'app/html', src: ['unword/**'], dest: 'dist/html/'},
-          {expand: true, cwd: 'app/styles', src: ['**'], dest: 'dist/styles/'}
+          {expand: true, cwd: 'app/images', src: ['**'], dest: 'dist/images/'},
+          {expand: true, cwd: 'app/', src: 'manifest.json', dest: 'dist/'},
         ]
       }
     }
@@ -48,9 +91,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean', 'copy', 'uglify', 'processhtml']);
+  grunt.registerTask('default', ['clean', 'copy', 'uglify', 'cssmin', 'processhtml']);
 
 };
